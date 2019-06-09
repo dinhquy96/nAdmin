66e579864be0d429e5c8dbe3504f8370f7e1a925
dinhquy96 <39159800+dinhquy96@users.noreply.github.com>
2019-06-09 21:32:29 +0700

    a
    a

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
   function add(){
-    	var d = new Date();
-    	//alert(d);
+      var d = new Date();
+      //alert(d);
         //alert(row);
          var id = document.getElementById("id").value;
         var title = document.getElementById("title").value;
         var author = document.getElementById("author").value;
           var category = document.getElementById("category").value;
+           var files = sessionStorage.getItem('img');
          var content = document.getElementById("content").value;
         var date = document.getElementById("date").value;
 
         if( title != '' && author != '' && content != '' && date != '' ){
-        	//addSession(nickname1);
-        	$('#table').prepend(
-         	`<tr>
+          //addSession(nickname1);
+          $('#table').prepend(
+          `<tr>
                         <td>`+id+`</td>
                         <td>`+ author +`</td>
                         <td>`+category+`</td>
                         <td>` +title+ `</td>
+                         <td><img src="`+ files +`" style="width: 70px; height: 70px;"></td>
                         <td>`+ content+ `</td>
                         <td>` + date+ `</td>
                         <td>
@@ -24,13 +26,13 @@
                          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal1" data-whatever="@fat"  > <i class="fa fa-edit"></i></button>  
                           <a class="btn btn-danger" href="#" ><i class="fa fa-trash"></i></a> </td>
                           </tr>`
-         	);
+          );
 
-         	//alert(sessionStorage.getItem("yourName"));
+          //alert(sessionStorage.getItem("yourName"));
         //window.location= 'post.html#comments';
         $("#success-alert").show();
         window.setTimeout(function (){
-        	$("#success-alert").hide();
+          $("#success-alert").hide();
         },2000);
         document.getElementById("id").value = '';
         document.getElementById("title").value = '';
@@ -40,23 +42,13 @@
         document.getElementById("date").value = '';
         }
         else{
-        	 $("#success-alert2").show();
+           $("#success-alert2").show();
         window.setTimeout(function (){
-        	$("#success-alert2").hide();
+          $("#success-alert2").hide();
         },2000);
         }   
     }
-      $('#tableCa').on('click', '.fa-trash', function (e) {
-                var selectedRow = $(this).closest('tr');
-                selectedRow.remove();
-                $("#success-alert3").show();
-            window.setTimeout(function (){
-              $("#success-alert3").hide();
-            },2000);
-            });
 
-
-      
      function addCa(){
         var d = new Date();
        // alert(d);
@@ -159,8 +151,55 @@
         },2000);
         }   
     }
+
+    function addRole(){
+        var d = new Date();
+        //alert(d);
+        //alert(row);
+          var nameRole = document.getElementById("nameRole").value;
+         var description = document.getElementById("description").value;
+       
+       //var admin = $("input[name='drone']:checked").val();
+        //alert(admin);
+        if( nameRole != '' && description != '' ){
+            //addSession(nickname1);
+            $('#tab').prepend(
+            `<tr>
+      <td>`+nameRole+`</td>
+      <td>`+d+`</td>
+      <td></td>
+      <td>`+description+`</td>
+      <td>
+        <div class="btn-group">
+                         <button type="button" onclick="showedit()"  class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@fat"  > <i class="fa fa-edit"></i></button>  
+                          <a class="btn btn-danger" href="#" ><i class="fa fa-trash"></i></a> 
+        </div>
+      </td>
+    </tr>`
+            );
+
+            //alert(sessionStorage.getItem("yourName"));
+        //window.location= 'post.html#comments';
+        $("#success-alert").show();
+        window.setTimeout(function (){
+            $("#success-alert").hide();
+        },2000);
+        // document.getElementById("id").value = '';
+        // document.getElementById("title").value = '';
+        // document.getElementById("author").value = '';
+        // document.getElementById("category").value = '';
+        // document.getElementById("content").value = '';
+        // document.getElementById("date").value = '';
+        }
+        else{
+             $("#success-alert2").show();
+        window.setTimeout(function (){
+            $("#success-alert2").hide();
+        },2000);
+        }   
+    }
     // function addSession(val){
-    // 	sessionStorage.setItem("yourName", val);
+    //  sessionStorage.setItem("yourName", val);
     // }
     function showedit(){
                  //var selectedRow = $(this).closest('tr');
@@ -183,8 +222,8 @@
     }
     function edit(){
        
-                 var id = document.getElementById("id1").value;
-                var title = document.getElementById("title1").value;
+        var id = document.getElementById("id1").value;
+        var title = document.getElementById("title1").value;
         var author = document.getElementById("author1").value;
           var category = document.getElementById("category1").value;
          var content = document.getElementById("content1").value;
@@ -204,9 +243,6 @@
                           <a class="btn btn-danger" href="#" ><i class="fa fa-trash"></i></a> </td>
                           </tr>`
             );
-                 
-                
-
             //alert(sessionStorage.getItem("yourName"));
         //window.location= 'post.html#comments';
         $("#success-alert1").show();
@@ -236,72 +272,118 @@
                 document.getElementById("content1").value = content;
                 document.getElementById("date1").value = date;
 
-                $(this).closest('.highlight').find('td:nth-child(4)').val("ok");
+                //$(this).closest('.highlight').find('td:nth-child(4)').val("ok");
+                                   
+      });
+    });
+
+     $(function() {
+                $('#dtHorizontalExample').on('click', 'tbody tr', function(event) {
+                   // document.getElementById("postForm").action= "admin.php?controller=product_researcher&act=edit";
+
+                $(this).addClass('highlight').siblings().removeClass('highlight');
+                var id = $(this).closest('.highlight').find('td:nth-child(1)').text();
+                var title = $(this).closest('.highlight').find('td:nth-child(4)').text();
+                var author = $(this).closest('.highlight').find('td:nth-child(2)').text();
+                var category = $(this).closest('.highlight').find('td:nth-child(3)').text();
+                var content = $(this).closest('.highlight').find('td:nth-child(5)').text();
+                var date = $(this).closest('.highlight').find('td:nth-child(6)').text();
+               //alert(idProduct); 
+                document.getElementById("id1").value = id;
+                document.getElementById("title1").value = title;
+                document.getElementById("author1").value = author;
+                document.getElementById("category1").value = category;
+                document.getElementById("content1").value = content;
+                document.getElementById("date1").value = date;
+
+                //$(this).closest('.highlight').find('td:nth-child(4)').val("ok");
                                    
       });
     });
 
     function subscribe(){
-    	var yourName = document.getElementById("yourName").value;
+      var yourName = document.getElementById("yourName").value;
         var email = document.getElementById("email").value;
-    	if( yourName != '' && email != ''){
-    	 $("#success-alert1").show();
-    	  window.setTimeout(function (){
-        	$("#success-alert1").hide();
-        },2000);
-    	}
-    	else{
-    		$("#success-alert2").show();
+      if( yourName != '' && email != ''){
+       $("#success-alert1").show();
         window.setTimeout(function (){
-        	$("#success-alert2").hide();
+          $("#success-alert1").hide();
         },2000);
-    	}
+      }
+      else{
+        $("#success-alert2").show();
+        window.setTimeout(function (){
+          $("#success-alert2").hide();
+        },2000);
+      }
        
     }
     function reply(a){
         var name = sessionStorage.getItem("yourName");
-    		var reply = document.getElementById("reply"+a).value;
+        var reply = document.getElementById("reply"+a).value;
             var d = new Date();
 
  $('.reply'+ a).append(
-    		`<ul class="children">
-				<li class="comment">
-					<div class="comment-block">
-					<a href="#" class="image-avatar">
-						<img src="images/photos/avatar-1.jpg" data-ot-retina="images/photos/avatar-1@2x.jpg" alt="" title="" />
-					</a>
-					<div class="comment-text">
-						<span class="time-stamp right">` + d + `</span>
-							<strong class="user-nick"><a href="#">`+ name +`</a></strong>
-							<div class="shortcode-content">
-							<p>` + reply + `</p>
-															</div>
-														
-														</div>
-													</div>
-												</li>
-											</ul>`);
+        `<ul class="children">
+        <li class="comment">
+          <div class="comment-block">
+          <a href="#" class="image-avatar">
+            <img src="images/photos/avatar-1.jpg" data-ot-retina="images/photos/avatar-1@2x.jpg" alt="" title="" />
+          </a>
+          <div class="comment-text">
+            <span class="time-stamp right">` + d + `</span>
+              <strong class="user-nick"><a href="#">`+ name +`</a></strong>
+              <div class="shortcode-content">
+              <p>` + reply + `</p>
+                              </div>
+                            
+                            </div>
+                          </div>
+                        </li>
+                      </ul>`);
 
     }
-
+   
     $(document).ready (function(){
-    	$("#success-alert").hide();
-    	$("#success-alert1").hide();
-    	$("#success-alert2").hide();
-    	$("#success-alert3").hide();
-    	// $("#myWish").click(function showAlert(){
-    	// 	$("#success-alert").alert();
-    	// })
+      $("#success-alert").hide();
+      $("#success-alert1").hide();
+      $("#success-alert2").hide();
+      $("#success-alert3").hide();
+      //alert(email);
+   //   var email = sessionStorage.getItem("email");
+      //alert(email);
+      if(email == null )
+      {
+   //     window.location = 'https://hanu98.github.io/Admin/';
+      }
+      // $("#myWish").click(function showAlert(){
+      //  $("#success-alert").alert();
+      // })
     });
 
      $('#table').on('click', '.fa-trash', function (e) {
                 var selectedRow = $(this).closest('tr');
                 selectedRow.remove();
                 $("#success-alert3").show();
-		        window.setTimeout(function (){
-		        	$("#success-alert3").hide();
-		        },2000);
+            window.setTimeout(function (){
+              $("#success-alert3").hide();
+            },2000);
             });
+
+     $('#tableCa').on('click', '.fa-trash', function (e) {
+                var selectedRow = $(this).closest('tr');
+                selectedRow.remove();
+                $("#success-alert3").show();
+            window.setTimeout(function (){
+              $("#success-alert3").hide();
+            },2000);
+            });
+
+
+     function logout(){
+  sessionStorage.removeItem("email");
+  window.location = 'https://hanu98.github.io/Admin/';
+ }
 
 
      function handleFileSelect(evt) {
